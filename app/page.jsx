f92b0ea1758337ba01711516a6465d338ea9d23b
DreamerView <temirkhan.onyx@gmail.com>
2023-08-01 19:51:57 +0600

    servicess added

--- app/page.jsx
+++ app/page.jsx
@@ -12,6 +12,41 @@ const Page = () => {
             <WelcomePage />
             <PromoLine />
             <ExperienceStory />
+            <section className="listRow">
+                <div className="listRowBlock">
+                    <h1>My <b>Services</b></h1>
+                    <hr />
+                    <div className="listRowBlockRow">
+                        <div className="listRowBlockContent">
+                            <h2>UI/UX Design</h2>
+                            <hr />
+                            <div className="listBlockRow">
+                                <div className="listBlock2"></div>
+                                <div className="listBlock1"></div>
+                                <div className="listBlock"></div>
+                            </div>
+                        </div>
+                        <div className="listRowBlockContent">
+                            <h2>UI/UX Design</h2>
+                            <hr />
+                            <div className="listBlockRow">
+                                <div className="listBlock2"></div>
+                                <div className="listBlock1"></div>
+                                <div className="listBlock"></div>
+                            </div>
+                        </div>
+                        <div className="listRowBlockContent">
+                            <h2>UI/UX Design</h2>
+                            <hr />
+                            <div className="listBlockRow">
+                                <div className="listBlock2"></div>
+                                <div className="listBlock1"></div>
+                                <div className="listBlock"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
         </>
     )
