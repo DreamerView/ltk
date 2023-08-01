e675e27ac210b4dfa2f5988b1f05f14f20090420
DreamerView <temirkhan.onyx@gmail.com>
2023-08-01 12:14:46 +0600

    burger added

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -20,6 +20,9 @@ const HeaderModule = () => {
                 <div className="logo">
                     <img src="/images/logo.svg" alt="" />
                 </div>
+                <button className="burgerNav">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  height="24px"  viewBox="0 0 24 24"  width="24px"  fill="#FFFFFF">  <path d="M0 0h24v24H0V0z" fill="none" />  <path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z" /></svg>
+                </button>
                 <nav>
                     <ul>
                         <li><Link href="/">Example 1</Link></li>
