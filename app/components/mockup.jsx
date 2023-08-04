8410dbbc9c3015a1f7a142cea927e4c1d81deced
DreamerView <temirkhan.onyx@gmail.com>
2023-08-04 10:17:18 +0600

    mockup changed

--- app/components/mockup.jsx
+++ app/components/mockup.jsx
@@ -1,11 +1,13 @@
 const mockupBlock = () => {
     return(
         <section className="mockup">
+            <h1>Легко и Удобно <b>Управляйте Процессами</b></h1>
+            <p>Легкое управление процессами: автоматизация, оптимизация, безопасность данных. Достижение максимальной эффективности для вашего бизнеса.</p>
             <div className="mockupBlock">
-                <div className="mockUpConsole">
-                    <img src="/images/promo.jpg" alt="" />
-                </div>
-                <img src="/images/mockup.webp" alt="" />
+                    <div className="mockUpConsole">
+                        <img src="/images/interface.webp" alt="" />
+                    </div>
+                    <img src="/images/mockup.webp" alt="" />
             </div>
         </section>
     );
