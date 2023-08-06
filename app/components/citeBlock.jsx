9ff81c21c5b80bf72cbe462353ccd3892671bc25
DreamerView <temirkhan.onyx@gmail.com>
2023-08-06 13:36:12 +0600

    stepByStep block added

---
+++ app/components/citeBlock.jsx
@@ -0,0 +1,14 @@
+const citeBlock = () => {
+    return(
+        <section className="cite">
+            <div className="citeBlock">
+                <blockquote className="blockquote-8">
+                    <p>Главное из основополагающих составляющих нашей деятельности заключается в организации нижнего, грамотно организованного (полевого), уровня автоматизации технологических процессов, где основная задача заключается, в сборе данных необходимых для дальнейшей обработки, анализа и глобального управления всем процессом.</p>
+                    <cite>ТОО “Light Technology Kazakhstan”</cite>
+                 </blockquote>
+            </div>
+        </section>
+    );
+};
+
+export default citeBlock;
