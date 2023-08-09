4a247c49d27a537297e5b9355a620723e41decf5
DreamerView <temirkhan.onyx@gmail.com>
2023-08-09 12:08:07 +0600

    icons added

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#409cff"/>
+        <meta name="apple-mobile-web-app-title" content="Light Technology Kazakhstan"/>
+        <meta name="application-name" content="Light Technology Kazakhstan"/>
+        <meta name="msapplication-TileColor" content="#2d89ef"/>
+      </head>
       <body>
         <HeaderModule />
           {children}
