107c06fdcf1d96c25e6bcae3184c5a81dfb10a91
DreamerView <temirkhan.onyx@gmail.com>
2023-08-09 10:10:36 +0600

    header logo link added

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -39,9 +39,9 @@ const HeaderModule = () => {
     return(
         <header>
             <div className="headerBlock">
-                <div className="logo main-object1">
+                <a onClick={closeBurger} href="#" className="logo main-object1">
                     <img src="/images/logo.svg" alt="" />
-                </div>
+                </a>
                 <div className="main-object2">
                 <button className="burgerNav" onClick={openBurger}>
                     {burger===false?<svg  xmlns="http://www.w3.org/2000/svg"  height="24px"  viewBox="0 0 24 24"  width="24px"  fill="#FFFFFF">  <path d="M0 0h24v24H0V0z" fill="none" />  <path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z" /></svg>:
